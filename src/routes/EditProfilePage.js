ff268f039bd3ab9b863b063c3f5432ed693be3ea
Jayjay Naval <jayjaynaval02@gmail.com>
2023-05-13 21:17:53 +0800

    deleted editprofile pageDoctor since it's not necessary, added auth in edit profile biography added for doctor for now

--- src/routes/EditProfilePage.js
+++ src/routes/EditProfilePage.js
@@ -10,6 +10,8 @@ function EditProfilePage() {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [imageFile, setImageFile] = useState(null);
+  const [userRole, setUserRole] = useState(null);
+  const [biography, setBiography] = useState('');
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -27,6 +29,10 @@ function EditProfilePage() {
           setGender(userData.gender);
           setPhoneNumber(userData.phoneNumber);
           setImageUrl(userData.imageUrl);
+          setUserRole(userData.role);
+          if (userData.role === 'Doctor') {
+            setBiography(userData.biography || '');
+          }
         } else {
           console.log('No such document!');
         }
@@ -70,6 +76,7 @@ function EditProfilePage() {
           email: email,
           gender: gender,
           phoneNumber: phoneNumber,
+          biography: biography,
         });
         if (imageFile) {
           await handleUpdateImage();
@@ -81,92 +88,105 @@ function EditProfilePage() {
     }
   };
 
-  
   return (
     <div className="card">
-      
-     <h5 className="card-title" style={{textAlign: 'center'}}>
-  <i className="fas fa-user" style={{marginRight: '10px'}}></i>Edit Profile
-</h5>
 
-    <div className="card-body">
-    
-      <div className="avatar">
-        <img src={imageUrl} alt="Avatar" />
-        <label className="file-label">
-          <input type="file" onChange={handleImageChange} />
-          <span className="file-button">Upload Image</span>
-        </label>
-      </div>
-      <div>
-      
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="firstName"></label>
-            <input
-              type="text"
-              className="form-control"
-              id="firstName"
-              value={firstname}
-              onChange={(event) => setFirstName(event.target.value)}
-              placeholder="First Name"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName"></label>
-            <input
-              type="text"
-              className="form-control"
-              id="lastName"
-              value={lastname}
-              onChange={(event) => setLastName(event.target.value)}
-              placeholder="Last Name"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email"></label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              placeholder="Email"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="gender"></label>
-            <select
-              className="form-control"
-              id="gender"
-              value={gender}
-              onChange={(event)=> setGender(event.target.value)}
-            >
-              <option value="">Select Gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="phoneNumber"></label>
-            <input
-              type="tel"
-              className="form-control"
-              id="phoneNumber"
-              value={phoneNumber}
-              onChange={(event) => setPhoneNumber(event.target.value)}
-              placeholder="Phone Number"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">Save</button>
-        </form>
+      <h5 className="card-title" style={{ textAlign: 'center' }}>
+        <i className="fas fa-user" style={{ marginRight: '10px' }}></i>Edit Profile
+      </h5>
+
+      <div className="card-body">
+
+        <div className="avatar">
+          <img src={imageUrl} alt="Avatar" />
+          <label className="file-label">
+            <input type="file" onChange={handleImageChange} />
+            <span className="file-button">Upload Image</span>
+          </label>
+        </div>
+        <div>
+
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="firstName"></label>
+              <input
+                type="text"
+                className="form-control"
+                id="firstName"
+                value={firstname}
+                onChange={(event) => setFirstName(event.target.value)}
+                placeholder="First Name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName"></label>
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                value={lastname}
+                onChange={(event) => setLastName(event.target.value)}
+                placeholder="Last Name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email"></label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="gender"></label>
+              <select
+                className="form-control"
+                id="gender"
+                value={gender}
+                onChange={(event) => setGender(event.target.value)}
+              >
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="phoneNumber"></label>
+              <input
+                type="tel"
+                className="form-control"
+                id="phoneNumber"
+                value={phoneNumber}
+                onChange={(event) => setPhoneNumber(event.target.value)}
+                placeholder="Phone Number"
+              />
+            </div>
+            {userRole === "Doctor" && (
+  <div className="form-group">
+    <label htmlFor="biography"></label>
+    <textarea
+      className="form-control"
+      id="biography"
+      value={biography}
+      onChange={(e) => setBiography(e.target.value)}
+      placeholder="Biography"
+      required
+    />
+  </div>
+)}
+
+            <button type="submit" className="btn btn-primary">Save</button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  
-);
+
+  );
 }
 
 export default EditProfilePage;
-             
+
